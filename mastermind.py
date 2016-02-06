ee0cfff0ed82cfeccb6c1f04406f3a082dd6d660
brettged <brettgedvilas@gmail.com>
2016-02-06 15:35:41 -0700

    Cleaning up playMastermind function and put in class

--- mastermind.py
+++ mastermind.py
@@ -7,19 +7,19 @@ Created on Tue Feb  2 18:59:28 2016
 import random
 
 class Peg(object):
-    
+
     def __init__(self, color):
         self.color = color
-        
+
     def __eq__(self, other):
         return self.color == other
-        
+
     def __str__(self):
         return self.color
-        
+
     def getColor(self):
         return self.color
-        
+
 black = Peg('black')
 white = Peg('white')
 red = Peg('red')
@@ -27,9 +27,9 @@ blue = Peg('blue')
 green = Peg('green')
 yellow = Peg('yellow')
 
-pegBank = [black, white, red, blue, green, yellow]       
-        
-        
+pegBank = [black, white, red, blue, green, yellow]
+
+
 # Have the computer choose a random code
 def randCode():
     code = []
@@ -43,91 +43,81 @@ def userCode():
     print "You have chosen to create the secret code! \n" \
            "The color choices are: \n \n" \
            " white \n black \n red \n blue \n yellow \n green \n"
-           
+
     color1 = Peg(raw_input("Enter color for first position: "))
     color2 = Peg(raw_input("Enter color for second position: "))
     color3 = Peg(raw_input("Enter color for third position: "))
     color4 = Peg(raw_input("Enter color for fourth position: "))
     code = [color1, color2, color3, color4]
     return code
-        
 
 class Guess(object):
-    
+
     def __init__(self, g1, g2, g3, g4):
-        
+
         self.guess = [Peg(g1), Peg(g2), Peg(g3), Peg(g4)]
-    
+
     def showGuess(self):
         for g in self.guess:
             print g
-            
+
     def iterGuess(self):
         return self.guess
-    
+
     def __eq__(self, other):
         return self.guess == other
 
 class Code(object):
-    
-    
+
     def __init__(self, code):
-        
+
         if code == 'cpu':
             self.code = randCode()
         elif code == 'user':
             self.code = userCode()
-            
+
     def showCode(self):
         for c in self.code:
             print c
 
     def colorCount(self):
+
         count = {}
-        
+
         for color in self.code:
             if color in count:
                 count[color] += 1
             else:
                 count[color] = 1
         return count
-        
+
     def checkGuess(self, code, guess):
-        
+
         codeCount = code.colorCount()
-        
-        clueList = []        
-        
+        clueList = []
         codeList = code.iterCode()
         guessList = guess.iterGuess()
-            
         guessIndex = 0
+
         for guessColor in guessList:
-            
             codeIndex = 0
             for codeColor in codeList:
-                
                 if guessColor == codeColor:
-                    
                     if guessIndex == codeIndex:
                         codeCount[codeColor] -= 1
-                        
                         if codeCount[codeColor] == 0:
                             del codeCount[codeColor]
-                        
                         clueList.append('red')
                 codeIndex += 1
             guessIndex += 1
 
-        
         for guessColor in guessList:
-
             if guessColor in codeCount.keys():
                 r = codeCount.keys().index(guessColor)
                 codeKey = codeCount.keys()[r]
                 clueList.append('white')
                 codeCount[codeKey] -= 1
-                
+
                 if codeCount[codeKey] == 0:
                     del codeCount[codeKey]
         print clueList
@@ -140,71 +130,66 @@ class Code(object):
 class MasterMind(object):
 
     def __init__(self):
-        pass
+        self.gameLog = []
 
+    def playMasterMind():
+        #game = MasterMind()
 
-def playMasterMind():
-    
-    print "Welcome to virtual MasterMind!"
-    raw_input("Press Enter to continue: ")
-    codeGen = raw_input("Would you like to play against the computer? (y or n) ")
+        numGuesses = 0
+        print "Welcome to virtual MasterMind!"
+        raw_input("Press Enter to continue: ")
+        codeGen = raw_input("Would you like to play against the computer? (y or n) ")
 
-    if codeGen == 'y':
-        codeGen = 'cpu'
-        code = Code(codeGen)
-        raw_input("The computer has created a code, please press Enter to begin guessing: ")
-    else:
-        codeGen = 'user'
-        code = Code(codeGen)
+        if codeGen == 'y':
+            codeGen = 'cpu'
+            code = Code(codeGen)
+            raw_input("The computer has created a code, please press Enter to begin " \
+                      "guessing: ")
+        else:
+            codeGen = 'user'
+            code = Code(codeGen)
 
-    if codeGen == 'cpu':
-        raw_input("The computer has created a code, please press Enter to begin guessing: ")
+        print "Color choices are: black, white, red, blue, green, and yellow \n"
+        while True:
 
-    print "Color choices are: black, white, red, blue, green, and yellow "
-    while True:
-        
-        g = raw_input("Enter Guess: ")
-        gParse = g.split()
-        
-        userGuess = Guess(gParse[0], gParse[1], gParse[2], gParse[3])
-        
-        code.checkGuess(code, userGuess)
-        
-        while userGuess != code:
-            g = raw_input("Next Guess: ")
+            g = raw_input("Enter Guess (or 'q' to quit): ")
+            if g == "q":
+                quit()
             gParse = g.split()
-            
             userGuess = Guess(gParse[0], gParse[1], gParse[2], gParse[3])
-            
             code.checkGuess(code, userGuess)
-                
-            if userGuess == code:
-                break
-        break
-    print "Congratulations! \n" \
-          "You solved the code!"
-    playAgain = raw_input("Would you like to play again? (y or n) ")
-    
-    if playAgain == 'y':
-        playMasterMind()
-        
-    else:
-        print "Goodbye!"
-    
-playMasterMind()
+            numGuesses += 1
+
+            while userGuess != code:
+                g = raw_input("Next Guess (or 'q' to quit: ")
+                if g == "q":
+                    quit()
+                gParse = g.split()
+                userGuess = Guess(gParse[0], gParse[1], gParse[2], gParse[3])
+                code.checkGuess(code, userGuess)
+                numGuesses += 1
+                if userGuess == code:
+                    self.gameLog.append(numGuesses)
+                    break
+            break
+        print "Congratulations! \n" \
+              "You solved the code in \n" \
+              "%s guesses" % numGuesses
+        playAgain = raw_input("Would you like to play again? (y or n) ")
+
+        if playAgain == 'y':
+            self.gameLog.playMasterMind()
+
+        else:
+            print "Goodbye!"
+
+    playMasterMind()
 
 
+def main():
 
+    game = MasterMind()
+    game.playMasterMind()
 
-
-
-
-
-
-
-
-
-
-        
-        
-        
+if __name__ == '__main__':
+    main()
